c18272afa15cc6690671156b62da1c0ad8b025ad
Pedro Angelo <pedro.fonini@smt.ufrj.br>
2016-02-23 05:35:09 -0300

    code generation pro filtro de 200Hz do VAD

--- build/config.sh
+++ build/config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-CURR_DIR=`pwd`
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+(cd ../codegen/filter200Hz/ && bash mkheader.sh)
+
 rm -rf debug release
 mkdir debug release
+
 cd debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../../
+
 cd ../release
 cmake -DCMAKE_BUILD_TYPE=Release ../../
-cd $CURR_DIR
