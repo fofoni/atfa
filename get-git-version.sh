8fa7c3f2f03295146732c09f495d7d159f4d7604
Pedro Angelo <pedro.fonini@smt.ufrj.br>
2016-06-08 12:17:13 -0300

    TODO no get-git-version.sh

--- get-git-version.sh
+++ get-git-version.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# when we get tags, make it possible to get a cleaner output, like the
-# current release (tag)
-
 #git log \
 #    --pretty="format:%ci, author: \"%aN\" <%aE>, commit: %h" -1 "${1}" || \
 #    echo no git \
 
-git describe || echo no git
+# TODO: esse arquivo deve ter um "export hook", ou algo assim, que faça
+#           com que ele seja substituído por um simples "echo versão".
+#       alternativa: criar um commit hook que substitui esse arquivo por
+#           um "echo versão" -> nao funciona, pq nao da pra saber o hash
+#           antes de commitar
+
+git describe || echo no-git
